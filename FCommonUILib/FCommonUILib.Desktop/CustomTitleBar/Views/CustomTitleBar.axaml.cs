20061d2b2678ba823768172677a773f0c0c3ffdf
Hesiodion <francoisfadel@gmail.com>
2025-04-05 19:25:45 +0200

    Adding Title Bar files

---
+++ FCommonUILib/FCommonUILib.Desktop/CustomTitleBar/Views/CustomTitleBar.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace FCommonUILib.FCommonUILib.Desktop.CustomTitleBar.Views
+{
+    public partial class CustomTitleBar : UserControl
+    {
+        public CustomTitleBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
