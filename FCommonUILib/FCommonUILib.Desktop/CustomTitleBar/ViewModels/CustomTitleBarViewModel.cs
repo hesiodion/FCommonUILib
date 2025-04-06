41b745076a793ed940c5b45b06205560c7779c38
Hesiodion <francoisfadel@gmail.com>
2025-04-06 16:45:43 +0200

    Adding some properties to the custom title bar

---
+++ FCommonUILib/FCommonUILib.Desktop/CustomTitleBar/ViewModels/CustomTitleBarViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Avalonia.Controls;
+using Avalonia.Threading;
+using ReactiveUI;
+using System;
+using System.Reactive;
+
+namespace FCommonUILib.FCommonUILib.Desktop.CustomTitleBar.ViewModels
+{
+    public class CustomTitleBarViewModel : ReactiveObject
+    {
+        public ReactiveCommand<Window, Unit> MinimizeCommand { get; }
+        public ReactiveCommand<Window, Unit> MaximizeRestoreCommand { get; }
+        public ReactiveCommand<Window, Unit> CloseCommand { get; }
+
+        public CustomTitleBarViewModel()
+        {
+            MinimizeCommand = ReactiveCommand.Create<Window>(window =>
+            {
+                if (window != null)
+                {
+                    ExecuteOnUIThread(() => window.WindowState = WindowState.Minimized);
+                }
+            }, outputScheduler: RxApp.TaskpoolScheduler);
+
+            MaximizeRestoreCommand = ReactiveCommand.Create<Window>(window =>
+            {
+                if (window != null)
+                {
+                    ExecuteOnUIThread(() => window.WindowState = window.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized);
+                }
+            }, outputScheduler: RxApp.TaskpoolScheduler);
+
+            CloseCommand = ReactiveCommand.Create<Window>(window =>
+            {
+                if (window != null)
+                {
+                    ExecuteOnUIThread(() => window.Close());
+                }
+            }, outputScheduler: RxApp.TaskpoolScheduler);
+
+        }
+
+        public static void ExecuteOnUIThread(Action action)
+        {
+            Dispatcher.UIThread.InvokeAsync(action);
+        }
+    }
+}
--- FCommonUILib/FCommonUILib.Desktop/CustomTitleBar/Views/CustomTitleBar.axaml.cs
+++ FCommonUILib/FCommonUILib.Desktop/CustomTitleBar/Views/CustomTitleBar.axaml.cs
@@ -1,12 +1,24 @@
+using Avalonia;
 using Avalonia.Controls;
+using FCommonUILib.FCommonUILib.Desktop.CustomTitleBar.ViewModels;
 
 namespace FCommonUILib.FCommonUILib.Desktop.CustomTitleBar.Views
 {
     public partial class CustomTitleBar : UserControl
     {
+        public static readonly StyledProperty<string> CustomTextProperty =
+            AvaloniaProperty.Register<CustomTitleBar, string>(nameof(CustomText));
+
+        public string CustomText
+        {
+            get => GetValue(CustomTextProperty);
+            set => SetValue(CustomTextProperty, value);
+        }
+
         public CustomTitleBar()
         {
             InitializeComponent();
+            DataContext = new CustomTitleBarViewModel();
         }
     }
 }
